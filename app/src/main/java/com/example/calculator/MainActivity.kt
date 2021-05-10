aa29e71af2912f9d127db583965e3cfe5cb9a3fb
chummypixels <chummypixels@gmail.com>
2021-05-10 08:00:14 +0100

    improve on functionality

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    var lastNumeric = false
+    var lastDot = false
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onDigit(view: View) {
+        tvInput.append((view as Button).text)
+        lastNumeric = true
+
+    }
+
+    fun onOperator(view: View){
+        if (lastNumeric && !isOperatorAdded(tvInput.text.toString())){
+            tvInput.append((view as Button).text)
+            lastNumeric = false
+            lastDot = false
+        }
+    }
+
+    private fun isOperatorAdded(value: String) : Boolean {
+        return if(value.startsWith("-")){
+            false;
+        }else{
+            value.contains("/") || value.contains("*") || value.contains("+") ||
+                    value.contains("-")
+        }
+    }
+
+    fun onClear(view: View) {
+        tvInput.text = ""
+        lastNumeric = false
+        lastDot = false
+    }
+
+    fun onDecimalPoint(view: View) {
+        if(lastNumeric && !lastDot){
+            tvInput.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+}
